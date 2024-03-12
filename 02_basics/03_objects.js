d5d6300009a79ff00b0d72be8631fe4276467c88
Subha Nayak <subhanayak022@gmail.com>
2024-03-12 20:24:24 +0530

    comment

--- 02_basics/03_objects.js
+++ 02_basics/03_objects.js
@@ -1,4 +1,4 @@
-// singleton
+// singleton: object jodi constructor diye toiri hoi
 // Object.create
 
 // object literals
@@ -9,7 +9,7 @@ const mySym = Symbol("key1")
 const JsUser = {
     name: "Hitesh",
     "full name": "Hitesh Choudhary",
-    [mySym]: "mykey1",
+    [mySym]: "mykey1", //Note how to define symbol
     age: 18,
     location: "Jaipur",
     email: "hitesh@google.com",
@@ -25,14 +25,19 @@ const JsUser = {
 JsUser.email = "hitesh@chatgpt.com"
 // Object.freeze(JsUser)
 JsUser.email = "hitesh@microsoft.com"
-// console.log(JsUser);
+console.log(JsUser);
 
 JsUser.greeting = function(){
     console.log("Hello JS user");
-}
+} 
+//[Function (anonymous)]: the funtion is not executed we have just got the reference of that function
 JsUser.greetingTwo = function(){
     console.log(`Hello JS user, ${this.name}`);
 }
-
+console.log(JsUser);
 console.log(JsUser.greeting());
 console.log(JsUser.greetingTwo());
+
+//using this we can add properties also in object
+JsUser.city = "kolkata"
+console.log(JsUser);
