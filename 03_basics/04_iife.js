2c8020f33cd09b227920fc309e5f24eb21ce79a6
Subha Nayak <subhanayak022@gmail.com>
2024-03-12 22:41:42 +0530

    c

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,5 +1,12 @@
 // Immediately Invoked Function Expressions (IIFE)
+// Encapsulation:
 
+// IIFE allows for encapsulation of variables and functions within a local scope, preventing them from polluting the global namespace.
+// Helps in avoiding variable conflicts and unintended modifications of global variables.
+// Avoiding Global Namespace Pollution:
+
+// JavaScript doesn't have block-level scoping (before ES6). Variables declared within blocks like loops or conditionals are accessible globally.
+// IIFE creates a private scope, ensuring variables within it do not leak into the global scope.
 
 (function chai(){
     // named IIFE
